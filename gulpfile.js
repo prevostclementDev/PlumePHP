8cb8da7e15d6e54bf1310759451ba654ad9a8727
prevostclementDev <clementprevost45@gmail.com>
2023-09-25 19:44:40 +0200

    fix(gulpfile.js): update version and task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,33 @@
+
+
+/*
+ * HOW USE GULP.
+ *
+ * To minify JS, and transform scss -> css.
+ * use command >gulp or >gulp default.
+ *
+ * To reset all minify JS and all scss transform css
+ * use command >gulp reset
+ *
+ * To delete all minify JS and scss transform to css
+ * use command >gulp delete
+ *
+*/
+
+
 /* ######################## */
 /* ###### IMPORTATION ##### */
 /* ######################## */
+const { gulp, series, parallel, watch, src, dest  } = require('gulp');
 
-/* GENERAL */
+const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
-const gulp = require('gulp');
+const babel = require('gulp-babel');
 
-/* CSS */
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 
-/* JS */
-const uglify = require('gulp-uglify');
+const del = require('delete');
 
 /* ################## */
 /* ###### FILES ##### */
@@ -34,25 +50,46 @@ const chemin = {
 /* ###### TASK ##### */
 /* ################# */
 
-/* TRANSFORM SCSS EN CSS */
-gulp.task('style', function(){
-    return gulp.src(chemin.styles.dev)
-    .pipe(sass())
-    .pipe(cleanCSS())
-    .pipe(rename({suffix : '-min'}))
-    .pipe(gulp.dest(chemin.styles.prod));
-});
+// default style export
+const defaultStyle = () => {
+    return src( chemin.styles.dev )
+        .pipe( sass() )
+        .pipe( cleanCSS() )
+        .pipe( rename( { suffix : '-min' } ) )
+        .pipe( dest( chemin.styles.prod ) );
+}
 
-/* TRANSFORM JS EN JS MINIFIER */
-gulp.task('script', function(){
-    return gulp.src(chemin.script.dev)
-    .pipe(uglify())
-    .pipe(rename({suffix : "-min"}))
-    .pipe(gulp.dest(chemin.script.prod));
-});
+// default js export
+const defaultJavaScript = () => {
+    return src( chemin.script.dev )
+        .pipe( babel() )
+        .pipe( uglify() )
+        .pipe( rename( {suffix : "-min"} ) )
+        .pipe( dest( chemin.script.prod) );
+}
 
-/* TACHE AUTO */
-gulp.task('default', function(){
-    gulp.watch(chemin.styles.Inspectdev,gulp.series('style')); // CSS
-    gulp.watch(chemin.script.dev,gulp.series('script')); // JS
-});
+// default watcher export
+function defaultGulpTaskWatcher(){
+    watch( chemin.styles.Inspectdev , defaultStyle );
+    watch( chemin.script.dev , defaultJavaScript );
+}
+
+
+exports.default = defaultGulpTaskWatcher;
+
+// delete all file
+const deleteAll = (cb) => {
+    del([chemin.script.prod+'*.js'],cb)
+    del([chemin.styles.prod+'*.css'],cb)
+}
+
+// reset export file
+exports.reset = (cb) => {
+    deleteAll(cb);
+
+    defaultStyle();
+    defaultJavaScript();
+};
+
+// delete all export file
+exports.delete = deleteAll;
